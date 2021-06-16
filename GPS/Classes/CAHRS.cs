6ee5597d70a9188f973e04a71935f0b9b82abe5d
KentStuff <thekentstuff@gmail.com>
2021-06-16 08:17:35 -0500

    Add project files.

---
+++ GPS/Classes/CAHRS.cs
@@ -0,0 +1,42 @@
+﻿
+namespace AgOpenGPS
+{
+    public class CAHRS
+    {
+        //private readonly FormGPS mf;
+
+        //Roll and heading from the IMU
+        public double imuHeading = 99999, prevIMUHeading = 0, imuRoll = 88888;
+
+        public System.Int16 angVel;
+
+        //actual value in degrees
+        public double rollZero;
+
+        //Roll Filter Value
+        public double rollFilter;
+
+        //is the auto steer in auto turn on mode or not
+        public bool isAutoSteerAuto, isRollInvert;
+
+        //the factor for fusion of GPS and IMU
+        public double fusionWeight;
+
+        //constructor
+        public CAHRS()
+        {
+            //mf = _f;
+
+            rollZero = Properties.Settings.Default.setIMU_rollZero;
+            //pitchZeroX16 = Properties.Settings.Default.setIMU_pitchZeroX16;
+
+            rollFilter = Properties.Settings.Default.setIMU_rollFilter;
+
+            isAutoSteerAuto = Properties.Settings.Default.setAS_isAutoSteerAutoOn;
+
+            fusionWeight = Properties.Settings.Default.setIMU_fusionWeight;
+            isRollInvert = Properties.Settings.Default.setIMU_invertRoll;
+        }
+    }
+}
+
